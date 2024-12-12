3712d87941bda3e0f417d53a60af6fcb0f99ecb7
Subhan Ul Haque <iamhaque2@gmail.com>
2024-12-13 02:11:00 +0530

    Solved Day 7 Part 2

--- day_07/main.ts
+++ day_07/main.ts
@@ -47,15 +47,48 @@ evalResult(
 
 /* Day 07 - Part 02 */
 
-// function part_02(input: string[]): number {
-//   return 0;
-// }
+function part_02(input: string[]): number {
+  let calibration_result = 0;
 
-// evalResult(
-//   7,
-//   2,
-//   part_02,
-// );
+  input.forEach((line) => {
+    const [a, b] = line.split(": ");
+    if (!b) return;
+
+    let can_be_evaluated = false;
+    const numbers = b.split(/\s+/);
+    const test_value = Number(a);
+    const operator_configurations = Math.pow(3, numbers.length - 1);
+    const binaryLength = (operator_configurations - 1).toString(3).length;
+
+    for (
+      let configuration = 0;
+      configuration < operator_configurations;
+      configuration++
+    ) {
+      const operator_configuration = configuration.toString(3).padStart(
+        binaryLength,
+        "0",
+      );
+
+      const result = evaluateNumbers(numbers, operator_configuration);
+
+      if (result === test_value) {
+        can_be_evaluated = true;
+        break;
+      }
+    }
+
+    if (can_be_evaluated) calibration_result += test_value;
+  });
+
+  return calibration_result;
+}
+
+evalResult(
+  7,
+  2,
+  part_02,
+);
 
 /* Shared functions */
 
@@ -66,10 +99,14 @@ function evaluateNumbers(
   let result = Number(numbers[0]);
 
   for (let index = 1; index < numbers.length; index++) {
-    if (operator_configuration[index - 1] === "0") {
+    const operator = operator_configuration[index - 1];
+
+    if (operator === "0") {
       result += Number(numbers[index]);
-    } else {
+    } else if (operator === "1") {
       result *= Number(numbers[index]);
+    } else if (operator === "2") {
+      result = Number(result.toString() + numbers[index]);
     }
   }
 
