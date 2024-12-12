20399737cf383768bad78e6d2799ce76055a8f45
Subhan Ul Haque <iamhaque2@gmail.com>
2024-12-13 01:06:55 +0530

    Started Day 7 Part 1

---
+++ day_07/main.ts
@@ -0,0 +1,27 @@
+import { evalResult } from "../utils.ts";
+
+/* Day 07 - Part 01 */
+
+function part_01(input: string[]): number {
+  return 0;
+}
+
+evalResult(
+  7,
+  1,
+  part_01,
+);
+
+/* Day 07 - Part 02 */
+
+function part_02(input: string[]): number {
+  return 0;
+}
+
+evalResult(
+  7,
+  2,
+  part_02,
+);
+
+/* Shared functions */
