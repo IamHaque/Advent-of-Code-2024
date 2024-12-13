eaede0ff022f5d7130181280f5bd9bbea1aa9991
Subhan Ul Haque <iamhaque2@gmail.com>
2024-12-13 21:25:37 +0530

    Solved Day 8 Part 2

--- day_08/main.ts
+++ day_08/main.ts
@@ -1,6 +1,6 @@
-import { evalResult } from "../utils.ts";
+import { evalResult } from '../utils.ts';
 
-const PATH = ".";
+const PATH = '.';
 
 type Position = {
   row: number;
@@ -14,7 +14,7 @@ type PositionAndKey = Position & {
 /* Day 08 - Part 01 */
 
 function part_01(input: string[]): number {
-  const map = input.map((line) => line.split(""));
+  const map = input.map((line) => line.split(''));
   const antennas = new Map<string, Position[]>();
   const antinodes = new Set<string>();
 
@@ -30,38 +30,52 @@ function part_01(input: string[]): number {
     const paths: string[] = getUniquePaths(positions);
 
     paths.map((path) => {
-      const [pos_1, pos_2] = findAntinodesForPath(path);
+      const [A, B] = getPositionsFromPath(path);
 
-      if (isInsideBounds(map, pos_1) && !antinodes.has(pos_1.key)) {
-        antinodes.add(pos_1.key);
-      }
-
-      if (isInsideBounds(map, pos_2) && !antinodes.has(pos_2.key)) {
-        antinodes.add(pos_2.key);
-      }
+      getAntinodesInDir(map, antinodes, A, B, 'prev');
+      getAntinodesInDir(map, antinodes, A, B, 'next');
     });
   });
 
   return antinodes.size;
 }
 
-evalResult(
-  8,
-  1,
-  part_01,
-);
+evalResult(8, 1, part_01);
 
 /* Day 08 - Part 02 */
 
-// function part_02(input: string[]): number {
-//   return 0;
-// }
+function part_02(input: string[]): number {
+  const map = input.map((line) => line.split(''));
+  const antennas = new Map<string, Position[]>();
+  const antinodes = new Set<string>();
 
-// evalResult(
-//   8,
-//   2,
-//   part_02,
-// );
+  map.forEach((row, rowIndex) => {
+    row.forEach((cell, colIndex) => {
+      if (cell === PATH) return;
+
+      if (!antennas.has(cell)) antennas.set(cell, []);
+      antennas.get(cell)!.push({ row: rowIndex, col: colIndex });
+
+      const cell_key = `${rowIndex},${colIndex}`;
+      if (!antinodes.has(cell_key)) antinodes.add(cell_key);
+    });
+  });
+
+  antennas.forEach((positions) => {
+    const paths: string[] = getUniquePaths(positions);
+
+    paths.map((path) => {
+      const [A, B] = getPositionsFromPath(path);
+
+      getAntinodesInDir(map, antinodes, A, B, 'prev', false);
+      getAntinodesInDir(map, antinodes, A, B, 'next', false);
+    });
+  });
+
+  return antinodes.size;
+}
+
+evalResult(8, 2, part_02);
 
 /* Shared functions */
 
@@ -90,25 +104,49 @@ function getUniquePaths(positions: Position[]): string[] {
   return paths;
 }
 
-function findAntinodesForPath(path: string): [PositionAndKey, PositionAndKey] {
-  const [pos_1, pos_2] = path.split("-");
+function getPositionsFromPath(path: string): [PositionAndKey, PositionAndKey] {
+  const [pos_1, pos_2] = path.split('-');
 
-  const [x_1, y_1] = pos_1.split(",").map((n) => Number(n));
-  const [x_2, y_2] = pos_2.split(",").map((n) => Number(n));
+  const [x_1, y_1] = pos_1.split(',').map((n) => Number(n));
+  const [x_2, y_2] = pos_2.split(',').map((n) => Number(n));
 
-  const A = { row: x_1, col: y_1 };
-  const B = { row: x_2, col: y_2 };
+  const A: PositionAndKey = { row: x_1, col: y_1, key: `${x_1},${y_1}` };
+  const B: PositionAndKey = { row: x_2, col: y_2, key: `${x_2},${y_2}` };
 
-  const C = findEquidistantPoint(A, B, A);
-  const D = findEquidistantPoint(A, B, B);
+  return [A, B];
+}
 
-  return [C, D];
+function getAntinodesInDir(
+  map: string[][],
+  antinodes: Set<string>,
+  pos_1: PositionAndKey,
+  pos_2: PositionAndKey,
+  direction: 'prev' | 'next',
+  get_once = true
+): void {
+  const new_pos = findEquidistantPoint(
+    pos_1,
+    pos_2,
+    direction === 'prev' ? pos_1 : pos_2
+  );
+
+  if (!isInsideBounds(map, new_pos)) return;
+
+  if (!antinodes.has(new_pos.key)) antinodes.add(new_pos.key);
+
+  if (get_once) return;
+
+  if (direction === 'prev') {
+    getAntinodesInDir(map, antinodes, new_pos, pos_1, direction, get_once);
+  } else {
+    getAntinodesInDir(map, antinodes, pos_2, new_pos, direction, get_once);
+  }
 }
 
 function findEquidistantPoint(
-  A: Position,
-  B: Position,
-  M: Position,
+  A: PositionAndKey,
+  B: PositionAndKey,
+  M: PositionAndKey
 ): PositionAndKey {
   const row = 2 * M.row - (A === M ? B.row : A.row);
   const col = 2 * M.col - (A === M ? B.col : A.col);
@@ -121,6 +159,10 @@ function findEquidistantPoint(
 }
 
 function isInsideBounds(map: string[][], position: PositionAndKey): boolean {
-  return position.row >= 0 && position.col >= 0 && position.row < map.length &&
-    position.col < map[0].length;
+  return (
+    position.row >= 0 &&
+    position.col >= 0 &&
+    position.row < map.length &&
+    position.col < map[0].length
+  );
 }
