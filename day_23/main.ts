c1f7311f3cefa65b63791cc8e7180f6798411388
Subhan Ul Haque <iamhaque2@gmail.com>
2024-12-29 21:01:47 +0530

    Solved Day 23 Part 2

--- day_23/main.ts
+++ day_23/main.ts
@@ -8,7 +8,7 @@ function part_01(input: string[]): number {
   const connections = new Set<string>();
 
   network.keys().forEach((source) => {
-    getConnectionOfLength(network, source, 3).forEach((conn) => {
+    getCircularConnections(network, source, 3).forEach((conn) => {
       if ((',' + conn).indexOf(',t') >= 0) connections.add(conn);
     });
   });
@@ -20,11 +20,24 @@ evalResult(23, 1, part_01);
 
 /* Day 23 - Part 02 */
 
-function part_02(input: string[]): number {
-  return 0;
+function part_02(input: string[]): string {
+  const network = getNetwork(input);
+
+  const cliques: Set<string>[] = [];
+
+  bronKerbosch(new Set(), new Set(network.keys()), new Set(), network, cliques);
+
+  const largestClique = cliques.reduce(
+    (max, clique) => (clique.size > max.size ? clique : max),
+    new Set<string>()
+  );
+
+  const password = [...largestClique].sort().join(',');
+
+  return password;
 }
 
-evalResult(23, 2, part_02);
+evalResult(23, 2, part_02, '_02');
 
 /* Shared functions */
 
@@ -46,17 +59,17 @@ function getNetwork(input: string[]): Map<string, Set<string>> {
   return network;
 }
 
-function getConnectionOfLength(
+function getCircularConnections(
   network: Map<string, Set<string>>,
   startNode: string,
-  length: number
+  length: number = 2
 ): Set<string> {
   const cycles = new Set<string>();
 
   function dfs(node: string, path: string[]) {
     path.push(node);
 
-    if (path.length === length + 1) {
+    if (path.length >= length + 1) {
       if (path[0] === path[path.length - 1]) {
         const sortedCycle = path.slice(0, -1).sort();
         cycles.add(sortedCycle.join());
@@ -72,4 +85,34 @@ function getConnectionOfLength(
 
   dfs(startNode, []);
   return cycles;
+}
+
+function bronKerbosch(
+  r: Set<string>,
+  p: Set<string>,
+  x: Set<string>,
+  graph: Map<string, Set<string>>,
+  cliques: Set<string>[]
+): void {
+  if (p.size === 0 && x.size === 0) {
+    cliques.push(new Set(r));
+    return;
+  }
+
+  const pivot = [...p][0] || [...x][0];
+  const pivotNeighbors = graph.get(pivot) || new Set();
+
+  [...p]
+    .filter((v) => !pivotNeighbors.has(v))
+    .forEach((v) => {
+      bronKerbosch(
+        new Set([...r, v]),
+        new Set([...p].filter((n) => graph.get(v)?.has(n))),
+        new Set([...x].filter((n) => graph.get(v)?.has(n))),
+        graph,
+        cliques
+      );
+      p.delete(v);
+      x.add(v);
+    });
 }
